7242eff37db470f9fd49e0ec34671fbe2de49b60
HosseinAlbakri <hosseinalbakri3@gmail.com>
2025-09-30 12:45:03 -0400

    added build folder to git ignore

--- build/_deps/googlebenchmark-build/third_party/googletest/googletest-paths.cmake
+++
@@ -1,2 +0,0 @@
-set(GOOGLETEST_SOURCE_DIR "/home/albakrih/Desktop/projects/sparse-dense-perf/build/_deps/googlebenchmark-build/third_party/googletest/src")
-set(GOOGLETEST_BINARY_DIR "/home/albakrih/Desktop/projects/sparse-dense-perf/build/_deps/googlebenchmark-build/third_party/googletest/build")
