0f66428942d06e9c14f9209bb6244dabdc8ad70f
HosseinAlbakri <hosseinalbakri3@gmail.com>
2025-10-02 17:09:30 -0400

    added ncu at the begining

--- build_run.sh
+++ build_run.sh
@@ -5,19 +5,11 @@
 #SBATCH --job-name="lab01"
 #SBATCH --nodes=1
 #SBATCH --output="lab01.%j.%N.out"
-#SBATCH -t 00:15:00
+#SBATCH -t 10:00:00
 ##################### SLURM (do not change) ^  #####################
 
 # Above are SLURM directives for job scheduling on a cluster,
 export SLURM_CONF=/etc/slurm/slurm.conf
-
-
-# Read input path from argument
-INPUT_PATH="$1"
-if [[ -z "$INPUT_PATH" ]]; then
-  echo "Error: No input path provided."
-  exit 1
-fi
 
 
 # build the benchmark
@@ -30,4 +22,8 @@ make -j8
 # Run the benchmark
 ./bin/spmm_benchmark  --benchmark_report_aggregates_only=true --benchmark_format=json > results_spmm_benchmark.json
 ./blocked_mm_bench  --benchmark_report_aggregates_only=true --benchmark_format=json > results_blocked_mm_bench.json
-./bin/sptrsv_benchmark "$INPUT_PATH" --benchmark_report_aggregates_only=true --benchmark_format=json > results_sptrsv_benchmark.json
+
+
+# Run the benchmark with profiling
+ncu --target-processes all --set full -o ./profiling_out_put_spmm_benchmark ./example_cuda/spmm_demo_gpu ./bin/spmm_benchmark  --benchmark_report_aggregates_only=true --benchmark_format=json > results_spmm_benchmark.json
+ncu --target-processes all --set full -o ./profiling_out_put_blocked_mm_bench ./example_cuda/spmm_demo_gpu ./blocked_mm_bench  --benchmark_report_aggregates_only=true --benchmark_format=json > results_blocked_mm_bench.json
