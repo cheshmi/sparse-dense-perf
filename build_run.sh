cef08cb8f4cd7349d0f1b51d41956a65b07d4a41
HosseinAlbakri <hosseinalbakri3@gmail.com>
2025-10-04 19:02:31 -0400

    change the build sh file and added taking the password from the input

--- build_run.sh
+++ build_run.sh
@@ -2,15 +2,23 @@
 
 ##################### SLURM (do not change) v  #####################
 #SBATCH --export=ALL
-#SBATCH --job-name="lab01"
+#SBATCH --job-name="project"
 #SBATCH --nodes=1
-#SBATCH --output="lab01.%j.%N.out"
+#SBATCH --output="project.%j.%N.out"
 #SBATCH -t 10:00:00
 ##################### SLURM (do not change) ^  #####################
 
 # Above are SLURM directives for job scheduling on a cluster,
 export SLURM_CONF=/etc/slurm/slurm.conf
 
+
+# Check for password argument
+if [ -z "$1" ]; then
+  echo "Usage: sbatch run.sh <sudo_password>"
+  exit 1
+fi
+
+SUDO_PASS="$1"
 
 # build the benchmark
 mkdir build
@@ -25,5 +33,5 @@ make -j8
 
 
 # Run the benchmark with profiling
-ncu --target-processes all --set full -o ./profiling_out_put_spmm_benchmark  ./bin/spmm_benchmark  --benchmark_report_aggregates_only=true --benchmark_format=json > results_spmm_benchmark_with_profiling.json
-ncu --target-processes all --set full -o ./profiling_out_put_blocked_mm_bench ./blocked_mm_bench  --benchmark_report_aggregates_only=true --benchmark_format=json > results_blocked_mm_bench_with_profiling.json
+echo "YOUR_PASSWORD" | sudo -S ncu --target-processes all --set full -o ./profiling_out_put_spmm_benchmark  ./bin/spmm_benchmark  --benchmark_report_aggregates_only=true --benchmark_format=json > results_spmm_benchmark_with_profiling.json
+echo "YOUR_PASSWORD" | sudo -S ncu --target-processes all --set full -o ./profiling_out_put_blocked_mm_bench ./blocked_mm_bench  --benchmark_report_aggregates_only=true --benchmark_format=json > results_blocked_mm_bench_with_profiling.json
