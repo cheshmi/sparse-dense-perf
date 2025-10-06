a53f097218b96f0f7eaf7f5af9e4dc682878f4f5
HosseinAlbakri <hosseinalbakri3@gmail.com>
2025-10-06 13:07:31 -0400

    updated the build_run file to share with the admin

--- build_run.sh
+++ build_run.sh
@@ -12,14 +12,6 @@
 export SLURM_CONF=/etc/slurm/slurm.conf
 
 
-# Check for password argument
-if [ -z "$1" ]; then
-  echo "Usage: sbatch run.sh <sudo_password>"
-  exit 1
-fi
-
-SUDO_PASS="$1"
-
 # build the benchmark
 mkdir build
 cd build
@@ -33,15 +25,15 @@ make -j8
 
 
 #Run the benchmark with profiling on nsys
-nsys profile -t cuda \
-             --output ./profiling_nsys_spmm_benchmark \
-             ./bin/spmm_benchmark  --benchmark_report_aggregates_only=true --benchmark_format=json > results_spmm_benchmark_nsys.json
+# nsys profile -t cuda \
+#              --output ./profiling_nsys_spmm_benchmark \
+#              ./bin/spmm_benchmark  --benchmark_report_aggregates_only=true --benchmark_format=json > results_spmm_benchmark_nsys.json
 
-nsys profile -t cuda \
-             --output ./profiling_nsys_blocked_mm_bench \
-             ./blocked_mm_bench  --benchmark_report_aggregates_only=true --benchmark_format=json > results_blocked_mm_bench_nsys.json
+# nsys profile -t cuda \
+#              --output ./profiling_nsys_blocked_mm_bench \
+#              ./blocked_mm_bench  --benchmark_report_aggregates_only=true --benchmark_format=json > results_blocked_mm_bench_nsys.json
 
 
 # Run the benchmark with profiling on nsight compute
-echo "$SUDO_PASS" | sudo -S ncu --target-processes all --set full -o ./profiling_out_put_spmm_benchmark  ./bin/spmm_benchmark  --benchmark_report_aggregates_only=true --benchmark_format=json > results_spmm_benchmark_with_profiling.json
-echo "$SUDO_PASS" | sudo -S ncu --target-processes all --set full -o ./profiling_out_put_blocked_mm_bench ./blocked_mm_bench  --benchmark_report_aggregates_only=true --benchmark_format=json > results_blocked_mm_bench_with_profiling.json
+ncu --target-processes all --set full -o ./profiling_out_put_spmm_benchmark  ./bin/spmm_benchmark  --benchmark_report_aggregates_only=true --benchmark_format=json > results_spmm_benchmark_with_profiling.json
+ncu --target-processes all --set full -o ./profiling_out_put_blocked_mm_bench ./blocked_mm_bench  --benchmark_report_aggregates_only=true --benchmark_format=json > results_blocked_mm_bench_with_profiling.json
